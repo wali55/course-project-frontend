590444fc4e95935c84fe281650f35958bb8c850e
wali55 <walisantunu@gmail.com>
2025-08-29 13:01:17 +0600

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -5,50 +5,20 @@ const api = axios.create({
   withCredentials: true,
 });
 
-let isRefreshing = false;
-let failedQueue = [];
-
-const processQueue = (error, token = null) => {
-  failedQueue.forEach((prom) => {
-    if (error) {
-      prom.reject(error);
-    } else {
-      prom.resolve(api(prom.request));
-    }
-  });
-
-  failedQueue = [];
-};
-
 api.interceptors.response.use(
   (response) => response,
   async (error) => {
     const originalRequest = error.config;
 
-    if (originalRequest.url.includes("/auth/refresh")) {
-      return Promise.reject(error);
-    }
-
     if (error.response?.status === 401 && !originalRequest._retry) {
-      if (isRefreshing) {
-        return new Promise((resolve, reject) => {
-          failedQueue.push({ resolve, reject, request: originalRequest });
-        });
-      }
-
       originalRequest._retry = true;
-      isRefreshing = true;
 
       try {
         await api.post("/auth/refresh");
-        processQueue(null); 
         return api(originalRequest);
       } catch (refreshError) {
-        processQueue(refreshError, null);
         window.location.href = "/login";
         return Promise.reject(refreshError);
-      } finally {
-        isRefreshing = false;
       }
     }
 
@@ -57,4 +27,3 @@ api.interceptors.response.use(
 );
 
 export default api;
-
